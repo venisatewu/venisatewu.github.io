7fa3377d882f54a073c7e04cfa5e850237f85ada
venisatewu <84917686+venisatewu@users.noreply.github.com>
2022-11-12 09:46:42 +0800

    Add new file

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.chat_icon').click(function(event){
+        $('.chat_box').toggleClass('active');
+    });
+})  
+
+// var init = function(){
+//     ('.chat_icon').click(function(event){
+//         ('.chat_box').toggleClass('active');
+// })}
+
+// $(document).ready(init);
+
